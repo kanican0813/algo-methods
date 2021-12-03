8e2387814110d907040a73fa4c094dce971df46b
Masa <masahiko.sugawara000@gmail.com>
2021-12-03 22:09:02 +0900

    update 215 answer

--- full_search/215.php
+++ full_search/215.php
@@ -1,1 +1,23 @@
 <?php
+
+/**
+ * https://algo-method.com/tasks/215
+ */
+
+$n = trim(fgets(STDIN));
+$a = explode(' ', trim(fgets(STDIN)));
+
+$i = 0;
+$cnt = 0;
+foreach ($a as $value) {
+    if($i === 0) {
+        $first = $value;
+        $i++;
+        continue;
+    }
+    if ($first < $value) {
+        $cnt++;
+    }
+    $first = $value;
+}
+echo $cnt . PHP_EOL;
