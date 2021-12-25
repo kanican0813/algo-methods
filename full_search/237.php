6fe7e7cba81d016c7705a7ec48fbd97714ade649
Masa <masahiko.sugawara000@gmail.com>
2021-12-25 22:26:45 +0900

    add question and answer

---
+++ full_search/237.php
@@ -0,0 +1,25 @@
+<?php
+$a = [];
+while(true) {
+    $e = trim(fgets(STDIN));
+    if(empty($e)) {
+        break;
+    } else {
+        array_push($a, $e);
+    }
+}
+
+$i = 1;
+$c = 0;
+foreach($a as $e) {
+    if ($i === 1) {
+        $i++;
+        continue;
+    }
+    $er = strrev($e);
+    if($e === $er) {
+        $c++;
+    }
+    $i++;
+}
+echo $c;
